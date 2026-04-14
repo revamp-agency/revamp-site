9188e7c9c74aaf9cf494d99459d8c117a488ac78
Elia Milan <elia@Elias-MacBook-Pro.local>
2026-04-14 17:20:03 +0800

    mountain 15th version

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import ServicesGrid from "@/components/sections/ServicesGrid";
 import CustomSoftware from "@/components/sections/CustomSoftware";
 import HowWeWork from "@/components/sections/HowWeWork";
 import WhyRevamp from "@/components/sections/WhyRevamp";
-import TechStack from "@/components/sections/TechStack";
 import FoundingClients from "@/components/sections/FoundingClients";
 import FAQ from "@/components/sections/FAQ";
 import FinalCTA from "@/components/sections/FinalCTA";
@@ -15,15 +14,14 @@ export default function Home() {
   return (
     <ScrollBackground>
       <Hero />
-      <PriceShock />
       <ServicesGrid />
       <CustomSoftware />
+      <PriceShock />
+      <FoundingClients />
       <HowWeWork />
       <WhyRevamp />
-      <TechStack />
-      <FoundingClients />
-      <FAQ />
       <FinalCTA />
+      <FAQ />
     </ScrollBackground>
   );
 }
